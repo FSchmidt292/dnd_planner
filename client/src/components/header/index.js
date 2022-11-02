9f16d2fe7e91b53605c423aabdc9020313fe8b63
Trey S <fjschmidt292@gmail.com>
2022-11-02 05:36:58 -0400

    beginning transferring starting code to react, goodnight

---
+++ client/src/components/header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth'
+
+const Header = () => {
+
+<header>
+  <ul>
+    <li>Home</li>
+    <li>logout</li>
+    <li>rules</li>
+  </ul>
+</header>
+
+};
+
+export default Header;
